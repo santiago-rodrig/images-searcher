35c659fd08b1cec5485a89845178fff3df1a2a1c
Santiago <santo1996.29@gmail.com>
2020-11-13 11:33:49 -0400

    feature: handle submit and change in Form

---
+++ src/components/ErrorComponent.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ErrorComponent = ({ children }) => (
+  <p className="my-3 p-4 text-center alert alert-primary">{children}</p>
+);
+
+export default ErrorComponent;
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,14 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
+import ErrorComponent from "./ErrorComponent";
 
 const Form = () => {
+  const [queryTerm, setQueryTerm] = useState("");
+  const [error, setError] = useState(false);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (queryTerm.trim() === "") {
+      setError(true);
+
+      return;
+    }
+
+    setError(false);
+  };
+
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <div className="row">
         <div className="form-group col-md-8">
           <input
             type="text"
             className="form-control form-control-lg"
             placeholder="Busca una imagen, ejemplo: futbol o cafe"
+            onChange={(e) => setQueryTerm(e.target.value)}
           />
         </div>
         <div className="form-group col-md-4">
@@ -19,6 +36,11 @@ const Form = () => {
           />
         </div>
       </div>
+      {error ? (
+        <ErrorComponent>
+          Por favor introduzca un término de búsqueda
+        </ErrorComponent>
+      ) : null}
     </form>
   );
 };
